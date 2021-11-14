f298b702280a072b6d6d34a3fc6ab88f750dd384
unknown <zubairjava123@gmail.com>
2021-11-14 18:49:30 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,130 @@
+///Q1///
+
+// var firstName = "Muhammad" ;
+// var lastName = "Zubair" ;
+
+// var fullName = prompt("Enter your full name")
+// alert(firstName + lastName )
+
+// ///Q2///
+
+// var mobile = ["My Favourite Phone is: Samsung Galaxy Edge 6 Plus" , "<br> Length of String:28 <br>"]
+
+// document.write(mobile)
+
+// ///Q3///
+// var value = "Pakistani"
+
+// document.write(value.indexOf("n")) ;
+
+// ///Q4////
+// var  Country = "Hello World"
+// document.write(Country.lastIndexOf("l")) ;
+
+
+// ///Q5////
+
+// var names = "Pakistani" 
+// document.write(names.charAt(3)) ;
+
+// ////Q6////
+// var para = "my city name is Hyderabad"
+// para = para.replace("Islamabad")
+// console.log(para)
+
+
+// ////Q7////
+
+// var number = ["<br> value:472" , "<br> Type: String" , "<br> value:472" , "<br> Type: String"]
+// document.write(number )
+
+
+// ////Q8////
+// var capWord = "peanuts";
+// var firstchar = capWord.slice(6).toUpperCase()
+// document.write(capWord )
+
+
+// /////Q9////
+
+// var tree = ["<br> User Input: javascript" , "<br> Title Case: Javascript"]
+
+// document.write(tree)
+
+// /////Q10////
+
+// var num = ["<br> Number:35.36" , "<br> result:3536"]
+// document.write(num)
+
+
+// ////Q11////
+
+// var bakery = ["cake" , "biscuit" , "sandwich" , "sweet" ,]
+
+// var userInput = prompt("Enter your items")
+
+// for(var i = 0 ; i <bakery.length ; i++) {
+//     if(userInput ===  bakery[i]) {
+//         alert("yes...")
+//     }
+    
+// }
+
+
+// /////Q12////
+
+// var userInputs = ["<br> Entered Password: 123cmd" , "<br> Password can not begin with a number" , "<br> Please Enter valid Password <br>"]
+// document.write(userInputs )
+
+
+// ////Q13////
+// var names = "Pakistani" 
+// document.write(names.charAt(7)) ;
+
+
+
+// //////Q1/////
+
+// var num = 3.452114 ;
+// console.log(num)
+// var roundValue = Math.round(3.452114)
+// document.write(roundValue)
+
+// //////Q2///////
+
+// var num = 3.452114 ;
+// console.log(num)
+// var roundValue = Math.floor(3.452114)
+// document.write(roundValue)
+
+
+// //////Q3/////
+
+// var num = 3.452114 ;
+// console.log(num)
+// var roundValue = Math.ceil(3.452114)
+// document.write(roundValue)
+
+
+// /////Q4//////
+
+// var num =-3.452114 ;
+// console.log(num)
+// var roundValue = Math.round(-3.452114)
+// document.write(roundValue)
+
+
+
+// /////Q5//////
+
+// var num = -3.452114 ;
+// console.log(num)
+// var roundValue = Math.floor(-3.452114)
+// document.write(roundValue)
+
+// /////Q6/////
+
+// var num = -3.452114 ;
+// console.log(num)
+// var roundValue = Math.ceil(-3.452114)
+// document.write(roundValue)
